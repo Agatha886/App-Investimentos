54daf98e7d5530ec0a87e18c21d8be587ec923e6
Agatha Monfredini de Paula Faria <agathafaria@brq.com>
2021-10-11 12:48:38 -0300

    Modificando alguns pontos na refatoração

--- app/src/main/java/br/com/brq/agatha/investimentos/repository/MoedaRepository.kt
+++
@@ -1,7 +0,0 @@
-package br.com.brq.agatha.investimentos.repository
-
-import br.com.brq.agatha.investimentos.viewModel.RetornoStadeApi
-
-interface MoedaRepository {
-    fun buscaDaApi(retornoStadeApi: (retorno: RetornoStadeApi) -> Unit)
-}
